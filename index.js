d816021efdf8cbccea5f3c31ec9ac84d82183a01
mehtachandrashekhar <67406458+mehtachandrashekhar@users.noreply.github.com>
2023-04-22 15:27:15 +0530

    updated html

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-document.getElementById("count-el").innerHTML = 5
+// document.getElementById("count-el").innerHTML = 5
+
+let count = 0;
+function increment(){
+    count=count+1;
+    document.getElementById("count-el").innerHTML=count;
+}
+
+function reset(){
+    count=0;
+    document.getElementById("count-el").innerHTML=count;
+}
+console.log(count);
